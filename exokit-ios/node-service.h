b1b43b35d37213c5a7a631121e2a38c164a94cf0
Avaer Kazmer <a@modules.io>
2018-02-11 13:30:53 -0500

    Update node service header

--- exokit-ios/node-service.h
+++ exokit-ios/node-service.h
@@ -1,6 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-void NodeService_start(const char *binPathString, const char *jsPathString, const char *libPathString, const char *urlString, const char *vrModeString, int vrTexture);
+void NodeService_onDrawFrame(float viewMatrixElements[], float projectionMatrixElements[], float centerArrayElements[]);
+void NodeService_start(const char *binPathString, const char *jsPathString, const char *libPathString, const char *dataPathString, const char *urlString, const char *vrModeString, int vrTexture, int vrTexture2);
+void NodeService_tick(int timeout);
 
 #endif
